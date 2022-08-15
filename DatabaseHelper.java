1d2d6cd10aad17102bc706d9a87b0adde2fe494e
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-08-15 17:35:26 +1000

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -8,8 +8,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
   public static final String DATABASE_NAME = "BicycleShop.db";
+  public static final String TABLE_NAME = "Bicycles";
+    public static final String COL_1 = "Brand";
+    public static final String COL_2 = "Model";
+    public static final String COL_3 = "Category";
+    public static final String COL_4 = "Price";
+  
   
   public DatabaseHelper(Context context){
     super(context, DATABASE_NAME, null, 1);
   }
+  
   
