b02d9089581a574c3d6c0d59b6e460bc8937c1cc
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-08-17 17:51:59 +1000

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -51,7 +51,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
   
       public Cursor getAllData(){
         SQLiteDatabase db=this.getWritableDatabase();
-        
+        Cursor cursor=db.rawQuery("select * from "+TABLE_NAME,null);
+                
     }
 
 }
